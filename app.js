9101b05d7a05c7e6158d427a628e63aa866765c4
Axelle <guinaudeau.ion1516@gmail.com>
2023-09-23 15:01:56 +0200

    add swagger

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
 const appointmentRoutes = require('./routes/appointment');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const app = express();
 const cors = require("cors");
 
 const corsOptions = {
-  origin: [process.env.ENVFRONT]
+  origin: [process.env.ENVFRONT, "http://localhost:3000"]
 }
+
+
+const optionsSwagger = {
+  definition: {
+    info: {
+      title: "MedicalReminder API",
+      version: "1.0.0",
+      description: "",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ],
+  },
+  apis: ["./routes/*.js"]
+}
+
+const specsSwagger = swaggerJsDoc(optionsSwagger)
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -20,6 +41,8 @@ app.use(
 app.use(bodyParser.json());
 app.use('/', userRoutes);
 app.use('/', appointmentRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specsSwagger));
 
 app.listen(3000);
 console.log('start port 3000');
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,136 @@ const user = require('../controllers/user');
 const auth = require("../middlewares/auth");
 const router = express.Router();
 
+/**
+ * tags:
+ *   - name: User Auth
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   User:
+ *     properties:
+ *       first_name:
+ *         type: string
+ *         example: John
+ *       last_name:
+ *         type: string
+ *         example: Doe
+ *       phone:
+ *         type: string
+ *         example: 0645483029
+ *       email:
+ *         type: string
+ *         example: john.doe@yopmail.com
+ *       password:
+ *         type: string
+ *         example: superSecretPassword1234
+ *       missed_appointments:
+ *         type: integer
+ *         example: 0
+ *       id_role:
+ *         type: integer
+ *         example: 5
+ *     required:
+ *       - first_name
+ *       - last_name
+ *       - phone
+ *       - email
+ *       - password
+ *       - missed_appointments
+ *       - id_role
+ */
+
+/**
+ * @swagger
+ * /create:
+ *   post:
+ *     tags:
+ *       - User Auth
+ *     description: Create user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: user
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *     responses: 
+ *       200 :
+ *         description: OK
+ *       default:
+ *         description: Unexpected error
+ *         
+ */
 router.post('/create', user.create);
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     tags:
+ *       - User Auth
+ *     description: Log the user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: login user
+ *         description: Receive an sms to get a code for the authentifaction
+ *         schema:
+ *           type: object
+ *           required:
+ *             - email
+ *             - password
+ *           properties:
+ *             email:
+ *               type: string
+ *             password:
+ *               type: string
+ *     responses: 
+ *       200 :
+ *         description: Return id user and you received a code by sms that you need to check in route /verify 
+ *       401 : 
+ *         description: Invalid identifiers
+ *       default:
+ *         description: Unexpected error
+ *         
+ */
 router.post('/login', user.login);
+
+/**
+* @swagger
+* /verify:
+*   post:
+*     tags:
+*       - User Auth
+*     description: Get token user
+*     produces:
+*       - application/json
+*     parameters:
+*       - in: body
+*         name: verify user
+*         description: Check your sms code and get the user token
+*         schema:
+*           type: object
+*           required:
+*             - id
+*             - phone
+*           properties:
+*             id:
+*               type: integer
+*             phone:
+*               type: integer
+*     responses: 
+*       200 :
+*         description: Return user token and his role
+*       default:
+*         description: Unexpected error
+*/
 router.post('/verify', user.verify);
+
+
 router.post('/logout', [auth], user.logout);
 router.post('/reset-password', user.resetPassword);
 router.post('/change-password', user.changePassword);
